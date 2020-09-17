06db5131d242df1954f1cf1f8b8f4f0118824660
wybosys <wybosys@gmail.com>
2020-09-17 15:06:20 +0800

    prj

--- src/nnt/thirds/dra/signal-protocol/crypto.ts
+++ src/nnt/thirds/dra/signal-protocol/crypto.ts
@@ -1,5 +1,9 @@
-import {KeyPair, PrivateKey, PublicKey} from "./keypair";
+import {IdentityKeyPair, KeyPair, PrivateKey, PublicKey} from "./keypair";
 import {FixedBuffer1, FixedBuffer16, FixedBuffer32, FixedBuffer64, FixedBuffer8} from "../../../core/buffer";
+import {Random} from "../../../core/random";
+import {MAX_INT} from "../../../core/kernel";
+import {PreKeyRecord} from "./state/prekeyrecord";
+import {SignedPreKeyRecord} from "./state/signedprekeyrecord";
 import nacl = require("tweetnacl");
 
 export type AesKeyBuffer = FixedBuffer32;
@@ -19,6 +23,50 @@ export class Crypto {
         r.pub = new PublicKey(new FixedBuffer32(kp.publicKey));
         r.priv = new PrivateKey(new FixedBuffer64(kp.secretKey));
         return r;
+    }
+
+    static GenerateRegistrationId(extendedRange?: boolean): number {
+        if (extendedRange) {
+            return Random.Rangei(0, MAX_INT) + 1;
+        }
+        return Random.Rangei(0, 16380) + 1;
+    }
+
+    static GetRandomSequence(max: number) {
+        return Random.Rangei(0, max);
+    }
+
+    static GeneratePreKeys(start: number, count: number): PreKeyRecord[] {
+        let r: PreKeyRecord[] = [];
+
+        for (let i = 0; i < count; ++i) {
+            let rcd = PreKeyRecord.Create(
+                start + i,
+                this.GenerateKeyPair());
+            r.push(rcd);
+        }
+
+        return r;
+    }
+
+    static GenerateSignedPreKey(identityKeyPair: IdentityKeyPair, signedPreKeyId: number): SignedPreKeyRecord {
+        let keyPair = this.GenerateKeyPair();
+        let signature = this.Sign(identityKeyPair.priv.forSerialize.buffer, keyPair.priv);
+        let now = new Date().getTime();
+        return SignedPreKeyRecord.Create(signedPreKeyId, now, keyPair, signature);
+    }
+
+    static GenerateSenderSigningKey(): KeyPair {
+        return this.GenerateKeyPair();
+    }
+
+    static GenerateSenderKey(): FixedBuffer32 {
+        let r = nacl.randomBytes(32);
+        return new FixedBuffer32(r);
+    }
+
+    static GenerateSenderKeyId(): number {
+        return Random.Rangei(0, MAX_INT);
     }
 
     static Sign(buf: Buffer, key: PrivateKey): SignatureBuffer {
--- src/nnt/thirds/dra/signal-protocol/groups/groupsessionbuilder.ts
+++ src/nnt/thirds/dra/signal-protocol/groups/groupsessionbuilder.ts
@@ -1,1 +1,47 @@
+import {SenderKeyStore} from "./state/senderkeystore";
+import {SenderKeyName} from "./senderkeyname";
+import {SenderKeyDistributionMessage} from "../protocol/senderkeydistributionmessage";
+import {Crypto} from "../crypto";
 
+export class GroupSessionBuilder {
+
+    private _senderKeyStore: SenderKeyStore;
+
+    constructor(store: SenderKeyStore) {
+        this._senderKeyStore = store;
+    }
+
+    async process(senderKeyName: SenderKeyName, senderKeyDistributionMessage: SenderKeyDistributionMessage) {
+        let senderKeyRecord = await this._senderKeyStore.loadSenderKey(senderKeyName);
+        senderKeyRecord.addSenderKeyState(
+            senderKeyDistributionMessage.id,
+            senderKeyDistributionMessage.iteration,
+            senderKeyDistributionMessage.chainKey,
+            senderKeyDistributionMessage.signatureKey
+        );
+        await this._senderKeyStore.storeSenderKey(senderKeyName, senderKeyRecord);
+    }
+
+    async create(senderKeyName: SenderKeyName): Promise<SenderKeyDistributionMessage> {
+        let senderKeyRecord = await this._senderKeyStore.loadSenderKey(senderKeyName);
+
+        if (senderKeyRecord.isEmpty()) {
+            senderKeyRecord.setSenderKeyState(
+                Crypto.GenerateSenderKeyId(),
+                0,
+                Crypto.GenerateSenderKey(),
+                Crypto.GenerateSenderSigningKey()
+            );
+
+            await this._senderKeyStore.storeSenderKey(senderKeyName, senderKeyRecord);
+        }
+
+        let state = senderKeyRecord.getSenderKeyState();
+        return SenderKeyDistributionMessage.Create(
+            state.keyId,
+            state.getSenderChainKey().iteration,
+            state.getSenderChainKey().seed,
+            state.signingKeyPublic
+        );
+    }
+}
